479c31548cec16cbf0c00987fc70a32b3a8dd344
gilbert-noriega-ii <gilbert.noriega.ii@gmail.com>
2021-02-12 23:28:02 -0600

    inital commit to github with table and fields

---
+++ full_text_search_extensions.sql
@@ -0,0 +1,63 @@
+/* actor table fields are 
+
+actor_id
+first_name
+last_name
+last_update
+
+*/
+
+/* customer table fields are 
+
+customer_id
+store_id
+first_name
+last_name
+email
+address_id
+activebool
+create_date
+last_update
+active
+
+*/
+
+/* rental table fields are 
+
+rental_id
+rental_date
+inventory_id
+customer_id
+return_date
+staff_id
+last_update
+
+*/
+
+
+/* film table fields are 
+
+film_id
+title
+description
+release_year
+language_id
+original_language_id
+rental_duration
+rental_length
+replacement_cost
+rating
+last_update
+special_features
+
+*/
+
+
+/* inventory table fields are 
+
+inventory_id
+film_id
+store_id
+last_update
+
+*/
